806e26db604646587292357d5cfc2fdcff30b3ec
black-sheepp <shivamguptanitw@gmail.com>
2025-02-03 20:36:03 +0530

    Sidebar Layout Created

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 // Source:
 
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/app-sidebar";
+
 function App() {
-  return (
-    <main className="flex flex-col items-center justify-center h-screen">
-      
-    </main>
-  );
+	return (
+		<main className='flex flex-col items-center justify-center h-screen'>
+			<SidebarProvider>
+				<AppSidebar />
+				<main>
+					<SidebarTrigger />
+					{/* {children} */}
+				</main>
+			</SidebarProvider>
+		</main>
+	);
 }
 
 export default App;
